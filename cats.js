f227c048e5ad7936939f2c4b5e090d53db28b8cf
SamChav <sam@DESKTOP-6D5K7Q2.localdomain>
2022-07-14 09:01:49 -0700

    my first server codealong

---
+++ cats.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var http = require('http')
+var port = process.env.PORT || 8000;
+
+var server = http.createServer(function (req, res) {
+    if (req.method === 'GET' && req.url === '/cats') {
+        var cats = 'meow'
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(cats)
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not found');
+    }
+});
+
+
+server.listen(port, function() {
+    console.log('Listening on port', port);
+  });
